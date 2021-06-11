8b82fc779d7bf1ffbf5d5e3d0a5195227d71009a
JoaoV1t0r <joaovitor.silva2pereira@gmail.com>
2021-06-10 21:43:51 -0300

    ciclo de vida dos componentes

--- src/App.js
+++ src/App.js
@@ -1,52 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-import { Component } from 'react';
+import "./App.css";
+import { Component } from "react";
 
-class App extends Component{
-
-  constructor(props){
-    super(props);
-    this.handlePClick = this.handlePClick.bind(this);
-    this.state = {
-      name: 'Vitor',
-      counter: 0
-    };
+class App extends Component {
+  state = {
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: "O título 1",
+        body: "O corpo 1",
+      },
+      {
+        id: 2,
+        title: "O título 2",
+        body: "O corpo 2",
+      },
+      {
+        id: 3,
+        title: "O título 3",
+        body: "O corpo 3",
+      },
+    ],
   };
 
-  handlePClick() {
-    // const {name} = this.state;
-    // console.log(`Click <p> ${name}`)
-    this.setState({name: 'João'});
+  timeOutUpdate = null;
+
+  componentDidMount() {
+    this.handleTimeOut();
+  }
+
+  componentDidUpdate() {
+    this.handleTimeOut();
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeOutUpdate);
+  }
+
+  handleTimeOut = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = "O título mudou";
+
+    this.timeOutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 });
+    }, 1000);
   };
 
-  handleAClick = (event) => {
-    event.preventDefault();
-    const {counter} = this.state;
-    this.setState({counter: counter + 1})
-    console.log(counter)
-  };
-
-  render(){
-    const {name} = this.state;
-     return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={this.handlePClick} >
-          Hello {name}!!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          onClick={this.handleAClick}
-        >
-          Este é o Link
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    const { posts, counter } = this.state;
+    return (
+      <div className="App">
+        <h1>{counter}</h1>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
