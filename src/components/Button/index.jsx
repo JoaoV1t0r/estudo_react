e0a17c5ee8a29912ecb93fd43d340f21639e44a5
JoaoV1t0r <joaovitor.silva2pereira@gmail.com>
2021-06-19 13:27:36 -0300

    aplicando testes

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,10 +4,10 @@ import "./style.css";
 
 export class Button extends Component {
   render() {
-    const { text, quandoClica, disabled } = this.props;
+    const { text, onClick, disabled } = this.props;
 
     return (
-      <button disabled={disabled} className="button" onClick={quandoClica}>
+      <button disabled={disabled} className="button" onClick={onClick}>
         {text}
       </button>
     );
